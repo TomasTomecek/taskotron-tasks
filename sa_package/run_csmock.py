1695011a653fde84416edd11c648ac3accb5728d
Tomas Tomecek <ttomecek@redhat.com>
2014-08-11 16:57:15 +0200

    improve sa task

--- sa_package/run_csmock.py
+++ sa_package/run_csmock.py
@@ -1,10 +1,31 @@
+import subprocess
+
 from libtaskotron import check
 
-def run_mytask(workdir, koji_build):
+
+CSMOCK_COMMAND = """\
+csmock -t cppcheck,gcc,clang %(srpm_path)s\
+"""
+
+
+def analyze(srpms):
+    for srpm in srpms:
+        cmd = CSMOCK_COMMAND % {'srpm_path': srpm}
+        rc = subprocess.call(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+def run(workdir, koji_build):
     print "Running mytask on %s" % koji_build
 
+    try:
+        analyze(koji_build['downloaded_rpms'])
+    except Exception:
+        result = 'FAILED'
+    except KeyError:
+        result = 'FAILED'
+    else:
+        result = 'PASSED'
+
     details = []
-    result = 'PASSED'
     summary = 'mycheck %s for %s' % (result, koji_build)
     report_type = check.ReportType.KOJI_BUILD
     detail = check.CheckDetail(koji_build, report_type,
